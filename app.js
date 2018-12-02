083650e975784484796686a07b4eff20ce02ed50
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-02 00:05:43 -0700

    add dependencies and error handling

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+function notFound(err, req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+};
+
+function errorHandler(err, req, res, next) {
+  console.error('Error', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+};
+
+app.listen(port, () => console.log(`Your server is on ${port}`));
